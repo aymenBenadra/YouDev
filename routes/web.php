e6fed98151d38ce4b7254dfd055388655caf64dd
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-20 11:37:14 +0000

    adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Models\{Offer, Project, User, Company};
+use App\Http\Controllers\OffersController;
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,29 +16,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
 
-// Projects routes
-Route::get('/projects', function () {
-    return Project::with('user')->get();
-});
+//? Login Routes
+Route::get('login/company', function () {
+    return view('company.login-company');
+})->name('login.company');
 
-// User Projects
-Route::get('/user/{user}/projects', function (User $user) {
-    return $user->projects;
-});
+Route::get('login', function () {
+    return view('user.login-user');
+})->name('login.user');
 
-// Offers routes
-Route::get('/offers', function () {
-    cache()->get('offers') ?? cache()->remember('offers', 60, function () {
-        return Offer::orderBy('id', 'desc')->get();
-    });
+//? Register Routes
+Route::get('register/company', function () {
+    return view('company.register-company');
+})->name('register.company');
 
-    return cache()->get('offers') ?? Offer::orderBy('id', 'desc')->get();
-});
+Route::get('register', function () {
+    return view('user.register-user');
+})->name('register.user');
 
-// Company Offers
-Route::get('/company/{company}/offers', function (Company $company) {
-    return $company->offers;
-});
+//? Projects routes
+Route::get('projects', [ProjectsController::class, 'index'])->name('projects');
+Route::get('projects/{project}', [ProjectsController::class, 'show'])->name('projects.show');
+Route::get('projects/create', [ProjectsController::class, 'create'])->name('projects.create');
+Route::get('projects/update/{project}', [ProjectsController::class, 'update'])->name('projects.update');
+
+//? Offers routes
+Route::get('offers', [OffersController::class, 'index'])->name('offers');
+Route::get('offers/{offer}', [OffersController::class, 'show'])->name('offers.show');
+Route::get('offers/create', [OffersController::class, 'create'])->name('offers.create');
+Route::get('offers/update/{offer}', [OffersController::class, 'update'])->name('offers.update');
